f706f2c33299415b1c9f13ee010f281b392d1f7f
known-001 <79784162+known-001@users.noreply.github.com>
2021-08-26 17:54:24 +1000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,12 @@
 server_script {
-    'server.lua',
-    'config.lua'
+    'config.lua',
+    'server.lua'
 }
 
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_scripts {
-    'client.lua',
-    'config.lua'
+    'config.lua',
+    'client.lua'
 }
 
 files {
